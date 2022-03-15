81210aad2d34d0e748b90c760671e5244b48e6c6
JoabPa <joabparedes.ec@gmail.com>
2022-03-15 00:00:09 -0400

    Running class added

--- script.js
+++ script.js
@@ -43,6 +43,23 @@ class Workout {
   }
 }
 
+class Running extends Workout {
+  type = 'running';
+
+  constructor(coords, distance, duration, cadence) {
+    super(coords, distance, duration);
+    this.cadence = cadence;
+    this.calcPace();
+    this._setDescription();
+  }
+
+  calcPace() {
+    // min/km
+    this.pace = this.duration / this.distance;
+    return this.pace;
+  }
+}
+
 class App {
   #map;
   #mapZoomLevel = 13;
